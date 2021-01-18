38aaa444544991d0109293ad2c0bfa33af83d817
Scott Fleming <sdflem@gmail.com>
2021-01-18 14:57:37 -0600

    Add an Empty Controller Test

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -1,7 +1,9 @@
 require "test_helper"
 
 class PagesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test 'should get about' do
+    
+  end
+
 end
