f7e6f502b1ce86ea4a5885ee68c6d27b797831e5
Scott Fleming <sdflem@gmail.com>
2021-01-23 13:11:47 -0600

    Run the Database Migration

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,2 +1,13 @@
+# == Schema Information
+#
+# Table name: todos
+#
+#  id          :bigint           not null, primary key
+#  description :text
+#  due_date    :date
+#  title       :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
 class Todo < ApplicationRecord
 end
--- test/models/todo_test.rb
+++ test/models/todo_test.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: todos
+#
+#  id          :bigint           not null, primary key
+#  description :text
+#  due_date    :date
+#  title       :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
 require "test_helper"
 
 class TodoTest < ActiveSupport::TestCase
