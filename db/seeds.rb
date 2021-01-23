df73f41ca1c739efb99ad28973a0d4ecbea88aa7
Scott Fleming <sdflem@gmail.com>
2021-01-23 13:02:37 -0600

    Seed the Database with Records

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Car.create!(
+  make: 'Dodge',
+  model: 'Viper',
+  color: 'Blue',
+  year: 1994,
+  price: 30700.70
+)
+
+Car.create!(
+  make: 'Chevy',
+  model: 'Equinox',
+  color: 'Gold',
+  year: 2015,
+  price: 8327.99
+)
+
+Car.create!(
+  make: 'Tesla',
+  model: 'Model X',
+  color: 'Black',
+  year: 2020,
+  price: 77641.00
+)
