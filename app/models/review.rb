b9fed0b10874b0e60f258bb2c1f6814c7c6c7eca
Scott Fleming <sdflem@gmail.com>
2021-01-29 13:00:19 -0600

    Run the Database Migration

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,17 @@
+# == Schema Information
+#
+# Table name: reviews
+#
+#  id           :bigint           not null, primary key
+#  body         :text
+#  genre        :string
+#  link         :string
+#  release_date :date
+#  review_date  :date
+#  score        :decimal(, )
+#  title        :string
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
 class Review < ApplicationRecord
 end
--- test/models/review_test.rb
+++ test/models/review_test.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: reviews
+#
+#  id           :bigint           not null, primary key
+#  body         :text
+#  genre        :string
+#  link         :string
+#  release_date :date
+#  review_date  :date
+#  score        :decimal(, )
+#  title        :string
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
 require "test_helper"
 
 class ReviewTest < ActiveSupport::TestCase
