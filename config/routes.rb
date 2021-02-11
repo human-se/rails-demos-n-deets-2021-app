b0717d770fafc7b6ca3b64436212a09d491f80b0
Scott Fleming <sdflem@gmail.com>
2021-02-11 08:11:57 -0600

    Add a New-Resource Route

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root to: redirect('/todos')
 
   get 'todos', to: 'todos#index', as: 'todos'
+  get 'todos/new', to: 'todos#new', as: 'new_todo'
   get 'todos/:id', to: 'todos#show', as: 'todo'
 
 end
