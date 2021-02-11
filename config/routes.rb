78a919b0ee05f3f17a20e966c4cf3c0edc48d96b
Scott Fleming <sdflem@gmail.com>
2021-02-11 08:17:06 -0600

    Add a Create-Resource Route

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root to: redirect('/todos')
 
   get 'todos', to: 'todos#index', as: 'todos'
+  post 'todos', to: 'todos#create'
   get 'todos/new', to: 'todos#new', as: 'new_todo'
   get 'todos/:id', to: 'todos#show', as: 'todo'
 
