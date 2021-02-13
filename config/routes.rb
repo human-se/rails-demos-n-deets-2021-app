95f492e17d93c167f78d70d82b4e68503030e397
Jeff Atkinson <jatkinson20@gmail.com>
2021-02-13 11:42:51 -0600

    Add an Edit-Resource Route

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root to: redirect('/todos')
 
   get 'todos', to: 'todos#index', as: 'todos'
+  get 'todos/:id/edit', to: 'todos#edit', as: 'edit_todo'
   get 'todos/:id', to: 'todos#show', as: 'todo'
 
 end
