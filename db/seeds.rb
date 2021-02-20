7f3e21c024c79319b6ee6d3cfd5f65a205c205d6
Scott Fleming <sdflem@gmail.com>
2021-02-20 12:06:57 -0600

    Fix Any Existing Code Broken by the New Association

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,21 +9,24 @@ quiz1_q1 = Question.create!(
   question: 'What city was the first capital of the United States?',
   answer: 'New York City',
   distractor_1: 'Washington, D.C.',
-  distractor_2: 'Boston'
+  distractor_2: 'Boston',
+  quiz: quiz1
 )
 
 quiz1_q2 = Question.create!(
   question: 'Who first discovered America?',
   answer: 'Leif Erikson',
   distractor_1: 'Christopher Columbus',
-  distractor_2: 'The Pilgrims'
+  distractor_2: 'The Pilgrims',
+  quiz: quiz1
 )
 
 quiz1_q3 = Question.create!(
   question: 'When was the Declaration of Independence signed?',
   answer: 'August 2nd, 1776',
   distractor_1: 'July 4, 1776',
-  distractor_2: 'September 3, 1783'
+  distractor_2: 'September 3, 1783',
+  quiz: quiz1
 )
 
 # Quiz 2
