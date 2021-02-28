e263cbdf1b2e0c9cf97910736a40737f23947420
Jeff Atkinson <jatkinson20@gmail.com>
2021-02-28 01:03:16 -0600

    base app complete

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,4 +11,12 @@ module ApplicationHelper
     bootstrap_alert_class[level]
   end
 
+  def active_class(path)
+    if request.path == path
+      return 'active'
+    else
+      return ''
+    end
+  end
+
 end
