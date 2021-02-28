313f19eb149c7f0ea0c6b3018ee38a466895107f
Jeff Atkinson <jatkinson20@gmail.com>
2021-02-28 01:19:06 -0600

    Require Sign-In for Controller Actions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,4 +1,5 @@
 class QuestionsController < ApplicationController
+  before_action :authenticate_user!
 
   def index
     @quiz = Quiz.find(params[:quiz_id])
--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -1,4 +1,5 @@
 class QuizzesController < ApplicationController
+  before_action :authenticate_user!, except: [:index]
 
   def index
     @quizzes = Quiz.all
